3c4d3adff71e86a296878219920d1b3cac91edb5
Salt Developer <salt-dev@Dev012.localdomain>
2022-03-29 16:48:00 +0200

    add logic to render happy or sad bunny

--- client/src/components/Garden.js
+++ client/src/components/Garden.js
@@ -1,9 +1,10 @@
+import { useSelector } from 'react-redux';
 import sprite0 from '../resources/level_000/bunny.png';
 import sprite1 from '../resources/level_001/background.gif';
 import sprite2 from '../resources/level_002/pumpkin.png';
 
 const Garden = () => {
-
+    const { carrots } = useSelector((state) => state.questions);
     //code that will be in slices
 
     const lvlDisplay = ( xp, lvl=0 ) => {
@@ -14,6 +15,7 @@ const Garden = () => {
     const currentLevel = (xp) => {
         const nb = [0];
         for (let i = 0; i < lvlDisplay(xp); i++) {
+            if (i===0) continue;
             nb.push(i);
         }
         return nb
@@ -21,14 +23,21 @@ const Garden = () => {
     // ------------------------------ //
     //TODO if carrot === 5, sad emote
     //TODO if carrot === 0, happy emote
-    const sprites = [sprite0, sprite1, sprite2];
+    const sprites = [{"happy": sprite0, "sad": sprite0}, sprite1, sprite2];
     return (
         <>
             <section className='garden__container'>
                 <p className="garden__username" >Pingu</p>
                 {
-                    currentLevel(0).map(nb => {
+                    currentLevel(12).map(nb => {
                         const sprite = sprites[nb]
+                        if (typeof sprite === "object") {
+                            if (carrots === 5) {
+                                return (<div key={nb} className={`garden--level${nb}`} style={{"background": `url(${sprite.sad})`}}></div>)
+                            } else {
+                                return (<div key={nb} className={`garden--level${nb}`} style={{"background": `url(${sprite.happy})`}}></div>)
+                            }
+                        }
                         if (sprite === sprite1) {
                             return(
                                 <div key={nb} className={`garden--level${nb}`} style={{"background": `url(${sprite}) `, "backgroundSize": "100%"}}></div>
