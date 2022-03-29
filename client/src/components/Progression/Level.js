97aa2a65b9f2c0d03458d2d6140311e1c13c18d5
salt-dev <salt-dev@salt-devs-MBP.localdomain>
2022-03-29 16:55:19 +0200

    add progression

---
+++ client/src/components/Progression/Level.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+
+
+
+const Level = () => {
+
+    return (
+    <>
+        <p>LV: 5</p>
+    </>)
+}
+
+export default Level;
