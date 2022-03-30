270904c6cae51580d7564182403c58ee6fa939da
Salt Developer <salt-dev@Dev012.localdomain>
2022-03-30 11:30:36 +0200

    update level route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose')
 const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
 
--- server/controllers/level.js
+++ server/controllers/level.js
@@ -1,4 +1,6 @@
+const mongoose = require('mongoose')
 const Level = require('../models/Level')
+const ErrorResponse = require('../utils/errorResponse')
 
 const createLevel = async (req, res, next) => {
     try {
@@ -24,14 +26,20 @@ const getLevel = async (req, res, next) => {
 }
 
 const updateLevel = async (req, res, next) => {
-    Level.findOneAndUpdate(
-        { id: req.body.id },
-        { xp: req.body.xp },
-        { perks : req.body.perks },
-        { new: true },
-        { lastConnected: Date.now()}
-    );
-    Level.save();
+    const { id } = req.params;
+    const { xp, carrotNumber, perks } = req.body;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return next(new ErrorResponse("no valid id", 404));
+    } 
+    const updateLevel = {
+        xp,
+        carrotNumber,
+        perks,
+        lastConnected: Date.now(),
+        _id: id
+    }
+    await Level.findByIdAndUpdate(id, updateLevel, { new: true });
+    res.json(updateLevel);
 }
 
 module.exports = {
