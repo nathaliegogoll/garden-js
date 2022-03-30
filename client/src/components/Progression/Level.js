36d63441bb83af68de4f2ab8e9e551c2acc0822d
Salt Developer <salt-dev@Dev012.localdomain>
2022-03-30 17:19:33 +0200

    add xp bar

--- client/src/components/Progression/Level.js
+++ client/src/components/Progression/Level.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Level = () => {
-    const currentLevel = useSelector((state) => state.user.user.level);
-    // const currentLevel = 100;
+    // const currentLevel = useSelector((state) => state.user.user.level);
+    const currentLevel = 100;
     console.log(currentLevel)
     return (
     <>
-        <p>LV: {currentLevel}</p>
+        <p className="level__level">LV: {currentLevel}</p>
     </>)
 }
 
-export default Level;
+export default Level; 
--- client/src/components/Progression/XP.js
+++ client/src/components/Progression/XP.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+const XP = () => {
+    const completed = 3;
+    const xpCap = (completed > 3) ?  5 : 3;
+    const remains = (completed - ((completed > 5) ?  3 : 0)) % ((completed > 5) ?  5 : 3);
+    const fullBar = (remains === 0 && completed !== 0) ? xpCap : remains ;
+    const percentageCompleted =  (fullBar * 100) / xpCap;
 
-
-
-const Level = () => {
-
+    const fillerStyles = {
+      height: '100%',
+      marginTop: '-1.2rem',
+      width: `${percentageCompleted}%`,
+      backgroundColor: "lightgreen",
+      transition: 'width 1s ease-in-out',
+      borderRadius: 'inherit',
+    }
+    const labelStyles = {
+      paddingLeft: "1rem",
+      color: 'black'
+    }
     return (
-    <>
-        <p>XP: 1/3</p>
-    </>)
-}
+      <div className="level__xpbar">
+        {`${fullBar} / ${xpCap} XP`}
+        <div style={fillerStyles}>
+        </div>
+      </div>
+    );
+  };
 
-export default Level;
+  export default XP
