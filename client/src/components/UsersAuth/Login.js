c6680b2f0742445a6958166e558e299d44c364ef
salt-dev <salt-dev@salt-devs-MBP.localdomain>
2022-04-06 09:53:13 +0200

    login styling

--- client/src/components/UsersAuth/Login.js
+++ client/src/components/UsersAuth/Login.js
@@ -35,13 +35,14 @@ const Login = () => {
     }
 
   return (
-    <section>
+    <section className='login__section'>
         <Title />
         <p className='title__description'>Adopt a bunny, solve Javascript katas to feed it and grow a beautiful pixel garden</p>
         <form className='login__form' autoComplete="off" onSubmit={handleSubmit}>
         <div className='form-group'>
             <label htmlFor='email'>Email</label>
             <input 
+            className='form__input'
             type='email'
             name='email'
             id='email'
@@ -52,8 +53,9 @@ const Login = () => {
             />
         </div>
         <div className='form-group'>
-            <label htmlFor='password'>password</label>
+            <label htmlFor='password'>Password</label>
             <input 
+            className='form__input'
             type='password'
             name='password'
             id='password'
